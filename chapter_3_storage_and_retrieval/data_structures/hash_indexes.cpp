06e0814d946eb5599a66c4f403cdd24b9b4bffae
Donaldo Cruz <donaldoacm@gmail.com>
2026-08-25 20:25:02 -0600

    C++ journey begins

---
+++ chapter_3_storage_and_retrieval/data_structures/hash_indexes.cpp
@@ -0,0 +1,10 @@
+// "#include" is a preprocessor directive that tells the compiler to include the standard Input/Output stream library in my program.
+// the "<>" angle brackets tell the compiler to look for the file within the standard system directories and not my local project folder.
+#include <iostream> 
+
+// using namespace std; // This is adding the "std" namespace to the list of namespaces that the compiler searches for names in. We are currently in the global namespace.
+
+int main() {
+    std::cout << "Starting to relearn C++.";
+    return 0;
+}
