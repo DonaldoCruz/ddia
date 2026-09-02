b80971ba779d011fb9b1485f791214a6035630b2
Donaldo Cruz <donaldoacm@gmail.com>
2026-09-01 20:47:04 -0600

    Learning C++ while making hash index

--- chapter_3_storage_and_retrieval/data_structures/hash_indexes.cpp
+++ chapter_3_storage_and_retrieval/data_structures/hash_indexes.cpp
@@ -1,10 +1,22 @@
 // "#include" is a preprocessor directive that tells the compiler to include the standard Input/Output stream library in my program.
 // the "<>" angle brackets tell the compiler to look for the file within the standard system directories and not my local project folder.
 #include <iostream> 
+#include <string>
 
 // using namespace std; // This is adding the "std" namespace to the list of namespaces that the compiler searches for names in. We are currently in the global namespace.
 
+std::string get(std::string data) {
+    std::string msg = "You are in the 'get' function.\nYou provided the following data:" + data;
+    std::cout << msg;
+    return msg;
+}
+
 int main() {
-    std::cout << "Starting to relearn C++.";
+    std::string data;
+    std::cin >> data;
+
+    std::string ex = get(data);
+    std::cout << "get function output:" + ex;
     return 0;
 }
+
