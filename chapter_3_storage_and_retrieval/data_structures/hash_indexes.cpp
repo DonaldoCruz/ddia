ef4324729e64ecfe82f58b32cb9e218f9f9893e5
Donaldo Cruz <donaldoacm@gmail.com>
2026-09-21 23:23:26 -0600

    Added put method and file writing.

--- chapter_3_storage_and_retrieval/data_structures/hash_indexes.cpp
+++ chapter_3_storage_and_retrieval/data_structures/hash_indexes.cpp
@@ -2,6 +2,7 @@
 // the "<>" angle brackets tell the compiler to look for the file within the standard system directories and not my local project folder.
 #include <iostream> 
 #include <string>
+#include <fstream>
 
 // using namespace std; // This is adding the "std" namespace to the list of namespaces that the compiler searches for names in. We are currently in the global namespace.
 
@@ -11,9 +12,19 @@ std::string get(std::string data) {
     return msg;
 }
 
+void put(std::string filename, std::string data) {
+    std::ofstream file; // Stream that we will write the data to. Output streams go like: Stream buffer (RAM) -> flush -> OS -> file/terminal/network/etc
+    // Input streams go like: OS -> buffer (RAM) -> flush -> RAM(variable in different location)
+    file.open(filename);
+    file.write(data.c_str(), data.size()); // data.c_str gives a pointer to an array that contains a null-terminated sequence of characters.
+    file.flush();
+    file.close();
+}
+
 int main() {
     std::string data;
     std::cin >> data;
+    put("test.txt", data);
 
     std::string ex = get(data);
     return 0;
